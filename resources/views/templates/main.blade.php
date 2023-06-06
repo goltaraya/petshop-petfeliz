df355bdcc2b1e176ea64405ecd26a8fcf3c75962
Yago Alexandre <yago.afonso@tcema.tc.br>
2023-06-06 12:04:07 -0300

    doing some routing stuffs

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -23,16 +23,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="/">Início</a>
+                        <a class="nav-link" href="{{ route('site.index') }}">Início</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/servicos">Serviços</a>
+                        <a class="nav-link" href="{{ route('site.services') }}">Serviços</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/sobre">Sobre</a>
+                        <a class="nav-link" href="{{ route('site.about') }} ">Sobre</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/contato">Contato</a>
+                        <a class="nav-link" href="{{ route('site.contact') }}">Contato</a>
                     </li>
                 </ul>
             </div>
