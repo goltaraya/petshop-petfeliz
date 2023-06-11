697ef2da3a773ddb1435124f00730f834cbc0908
Yago Alexandre <goltarayago@gmail.com>
2023-06-11 20:39:07 -0300

    fix: required fields in input forms - patients

--- resources/views/app/pacients/create.blade.php
+++ resources/views/app/pacients/create.blade.php
@@ -12,22 +12,22 @@
             @csrf
             <div class="mb-3">
                 <label for="name" class="form-label">Nome</label>
-                <input type="text" class="form-control" id="name" name="name">
+                <input required type="text" class="form-control" id="name" name="name">
             </div>
             <div class="mb-3">
                 <label for="birth" class="form-label">Data de Nascimento</label>
-                <input type="date" class="form-control" id="birth" name="birth">
+                <input required type="date" class="form-control" id="birth" name="birth">
             </div>
             <div class="mb-3">
                 <label for="species" class="form-label">Espécie</label>
-                <input type="text" class="form-control" id="species" name="species">
+                <input required type="text" class="form-control" id="species" name="species">
             </div>
             <div class="mb-3">
                 <label for="gender" class="form-label">Sexo</label>
                 <div class="gender-radio-option">
-                    <input type="radio" id="male" name="gender" value="macho" checked="checked">
+                    <input required type="radio" id="male" name="gender" value="macho" checked="checked">
                     <label for="male">Macho</label>
-                    <input type="radio" id="female" name="gender" value="fêmea">
+                    <input required type="radio" id="female" name="gender" value="fêmea">
                     <label for="female">Fêmea</label>
                 </div>
             </div>
